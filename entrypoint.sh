fa5dd8e2794226da66a00154b62283b2b4fc3b5f
charly-tricks <83989666+charly-tricks@users.noreply.github.com>
2025-10-28 21:27:18 -0300

    Fix Nginx startup command in entrypoint.sh
    Updated Nginx startup command to use full path.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 # 1. Reemplaza el marcador __PORT__ con la variable $PORT que nos da Cloud Run
-#    (Que en tu caso es 8080)
 # 2. Reemplaza el marcador __BACKEND_URL__ con la variable $PROXY_TARGETS
-#    (Que en tu caso es 77.237.232.1:5680)
 envsubst < /etc/nginx/conf.d/proxy_backend.template > /etc/nginx/conf.d/default.conf
 
-# Inicia Nginx en primer plano (obligatorio para Cloud Run)
-nginx -g 'daemon off;'
+# 3. Inicia Nginx usando su ruta completa (esta es la corrección)
+/usr/sbin/nginx -g 'daemon off;'
